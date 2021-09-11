b8ab2518542223c9c6e0108b06df581e1c14f824
Ana Eliza Feitoza <ana.eliza@alunos.ifsuldeminas.edu.br>
2021-09-10 23:47:48 -0300

    remover e validar pessoa

--- removerPessoa.php
+++ removerPessoa.php
@@ -6,51 +6,64 @@
 
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-		giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-		<title>Leitores - Remoção</title>
+		<title>Animais - Remoção</title>
 	</head>
 
   	<body>
 		<div class="container">
 			<header class="bg-primary text-white text-center">
-				<h1> Sistema WEB para Compra de Livros</h1>
+				<h1> Sistema WEB para Adoção de Animais</h1>
 			</header>
 
 			<hr>
 			<div class="row">
 				
 				<div class="col-2">
-					<a href="index.php" class="btn btn-primary">Página Inicial</a>
+					<a href="index.html" class="btn btn-primary">Página Inicial</a>
 				</div>
 			
 				<div class="col-2">
-					<a href="quem_somos.php" class="btn btn-primary">Quem Somos</a>
+					<a href="quem_somos.html" class="btn btn-primary">Quem Somos</a>
 				</div>
 				<div class="col-2">
-					<a href="livros.php" class="btn btn-primary">Livros</a>
+					<a href="animais.html" class="btn btn-primary">Animais</a>
 				</div>
 				<div class="col-2">
-					<a href="pessoas.php" class="btn btn-primary">Pessoas</a>
+					<a href="pessoas.html" class="btn btn-primary">Pessoas</a>
 				</div>
 				<div class="col-2">
-					<a href="login.php" class="btn btn-success">Login</a>
+					<a href="login.html" class="btn btn-success">Login</a>
 				</div>
 				<div class="col-2">
-					<a href="logout.php" class="btn btn-danger">Logout</a>
+					<a href="logout.html" class="btn btn-danger">Logout</a>
 				</div>
 			</div>
+
 			<hr>
 
-			<form method="POST" action="remover_pessoa_bd.php">
+			<form method="POST" action="#">
 			  <label for="pessoaEscolhida" class="form-label">Escolha qual pessoa você deseja remover do banco de dados: </label>
 			  <select class="form-select" id="pessoaEscolhida" name="pessoaEscolhida">
-				<option> Inês </option>
-				<option> Maria </option>
-				<option> Pedro Henrique </option>
-				<option> Sophia </option>
+				<?php
+					require_once "model/pessoa.php";
+					$pessoas = selecionarTodasPessoas();
+					foreach ($pessoas as $p)
+					{
+						echo "<option value = $p->id>" . $p->nome . "</option>";	
+					}
+				?>
 			  </select><br>
 			  <button type="submit" class="btn btn-primary">Remover pessoa </button>
 			</form>
 			
+			<?php
+				if (isset($_POST["pessoaEscolhida"]))
+				{
+					require_once "model/pessoa.php";
+					excluirPessoa($_POST["pessoaEscolhida"]);
+					echo "Pessoa excluída com sucesso do BD!";
+				}
+			?>
 
 			<hr>
 
--- validacao_pessoa.php
+++ validacao_pessoa.php
@@ -1,32 +1,31 @@
 <?php
-	        $erros="";
-	        if (!isset($_POST["nome"]) || !isset($_POST["endereco"]) || !isset($_POST["preferencias"]) || strlen($_POST["preferencias"]) == 0)
-	        {
-		    $erros .= "Todos os campos devem ser preenchidos <br>";
-	        }
-	        if ( strlen($erros) == 0)
-			{	
-				if (isset($_POST["idOculto"]))
-				{
-					require_once "model/pessoa.php";
-					atualizarPessoa($_POST["nome"], $_POST["endereco"], $_POST["preferencias"]);
-					echo "Atualização efetuada com sucesso! <br>";
-				}
-				else
-				{
-					require_once "model/pessoa.php";
-					atualizarPessoa($_POST["nome"], $_POST["endereco"], $_POST["preferencias"]);
-					echo "Cadastro efetuado com sucesso! <br>";
-				}
-			echo "Você será redirecionado para a página inicial do sistema.<br>";
-			header ("refresh:5;url=index.php");
-			}	
-
-	        else
-	        {
-		    echo $erros . "<br>";
-		    echo "Você será redirecionado para a página de cadastro.<br>";
-		    header ("refresh:5;url=cadastrarPessoa.html");
-	        }
-            ?>
+	$erros="";
+	if (!isset($_POST["nome"]) || !isset($_POST["endereco"]) || !isset($_POST["preferencias"]) || strlen($_POST["preferencias"]) == 0)
+	{
+		$erros .= "Todos os campos devem ser preenchidos <br>";
+	}
+	if ( strlen($erros) == 0)
+	{
+		if (isset($_POST["idOculto"]))
+		{
+			require_once "model/pessoa.php";
+			atualizarPessoa($_POST["nome"], $_POST["endereco"], $_POST["preferencias"], $_POST["idOculto"]);
+			echo "Atualização efetuada com sucesso! <br>";
+		}
+		else
+		{
+			require_once "model/pessoa.php";
+			inserirPessoa($_POST["nome"], $_POST["endereco"], $_POST["preferencias"]);
+			echo "Cadastro efetuado com sucesso! <br>";
+		}
+		echo "Você será redirecionado para a página inicial do sistema.<br>";
+		header ("refresh:5;url=index.html");
+	}
+	else
+	{
+		echo $erros . "<br>";
+		echo "Você será redirecionado para a página de cadastro.<br>";
+		header ("refresh:5;url=cadastrarPessoa.html");
+	}
+?>
 			
